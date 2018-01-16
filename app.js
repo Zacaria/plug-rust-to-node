ffbba23533f4a59100def0a690498636ba2aa226
Zacaria <z@havesomecode.io>
2018-01-16 14:36:07 +0100

    [UP] concat returns a string

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const ffi = require('ffi');
 
 const lib = ffi.Library(path.join(__dirname, './target/debug/libplug_node'), {
   multiply: ['int', ['int', 'int']],
-  concat: ['int', ['int', 'int']],
+  concat: ['string', []],
 });
 
 const result = lib.multiply(15, 10);
-const result2 = lib.concat(12, 3);
+const result2 = lib.concat();
 
 console.log(result, result2);
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 extern crate libc;
+use std::ffi::CString;
+use std::os::raw::c_char;
 
 #[no_mangle]
-
 pub extern "C" fn multiply (arg1: libc::c_int, arg2: libc::c_int) -> libc::c_int {
 
     arg1 * arg2
 }
 
 #[no_mangle]
-
-pub extern "C" fn concat (arg1: libc::c_int, arg2: libc::c_int) -> libc::c_int  {
-
-    arg1 * arg2
+pub extern "C" fn concat () -> *const c_char  {
+    let s = CString::new("Hello World").unwrap();
+    let p = s.as_ptr();
+    std::mem::forget(s);
+    p
 }
