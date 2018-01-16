5cf1c69e1ad1a96d14860f34795326f7ba45566d
Zacaria <z@havesomecode.io>
2018-01-16 15:45:19 +0100

    [ADD] neon to link rust and node

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const ffi = require('ffi');
 
-const lib = ffi.Library(path.join(__dirname, './target/debug/libplug_node'), {
-  multiply: ['int', ['int', 'int']],
-  concat: ['string', []],
-});
+const hello = require('./lib');
 
-const result = lib.multiply(15, 10);
-const result2 = lib.concat();
+// const lib = ffi.Library(path.join(__dirname, './native/target/debug/libplug_node'), {
+//   multiply: ['int', ['int', 'int']],
+//   concat: ['string', []],
+// });
+//
+// const result = lib.multiply(15, 10);
+// const result2 = lib.concat();
 
-console.log(result, result2);
+// console.log(result, result2);
+console.log(hello);
