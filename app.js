aad0c88020176209e5612c7c13dd5f2d6ef42190
Zacaria <z@havesomecode.io>
2018-01-16 12:04:53 +0100

    [ADD] concat function in the lib

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const ffi = require('ffi');
 
 const lib = ffi.Library(path.join(__dirname, './target/debug/libplug_node'), {
   multiply: ['int', ['int', 'int']],
+  concat: ['int', ['int', 'int']],
 });
 
-function multiply(a, b) {
-  return a * b;
-}
-
 const result = lib.multiply(15, 10);
+const result2 = lib.concat(12, 3);
 
-console.log(result);
+console.log(result, result2);
--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,10 @@ pub extern "C" fn multiply (arg1: libc::c_int, arg2: libc::c_int) -> libc::c_int
 
     arg1 * arg2
 }
+
+#[no_mangle]
+
+pub extern "C" fn concat (arg1: libc::c_int, arg2: libc::c_int) -> libc::c_int  {
+
+    arg1 * arg2
+}
